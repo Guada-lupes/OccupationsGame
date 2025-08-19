7f34dfa2492a67fc45d1c101a345deda24de7442
Guadalupe <gma.pedagoga@gmail.com>
2025-08-19 16:30:29 +0100

    delete trash

--- src/trash/openActivity.jsx
+++
@@ -1,8 +0,0 @@
-import { Link } from "react-router-dom";
-
-export function openActivity(elemento) {
-    //ruta dinamica que diriga al componente ActivityInfo pasando el parametro de elemento
-    return(
-        <Link to="/:id"></Link>
-    )
-}
--- src/trash/searchActivity.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export function searchActivity(id, objeto) {
-  const claveEncontrada = Object.keys(objeto).find(clave => clave === id);
-  return claveEncontrada ? objeto[claveEncontrada] : null;
-}
-
